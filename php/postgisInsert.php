5d47c6f6ead36db28d30c054d9deb4a8545f3faf
root <root@ip-172-31-17-198.us-west-2.compute.internal>
2014-08-08 01:19:46 +0000

    added the sha checking to the php script

--- php/postgisInsert.php
+++ php/postgisInsert.php
@@ -5,26 +5,28 @@
   //if(!$dbh){
   //  die("Error in connection: " . pg_last_error());
   //}
-  
-  if($_POST['submit']){
-    $dbh = pg_connect("dbname=$dbname user=$dbuser password=$dbpass");
-    if(!$dbh){
-      die("Error in connection: " . pg_last_error());
+
+  if($_POST['hash']=hash('sha256','$sensorpass')){  
+    if($_POST['submit']){
+      $dbh = pg_connect("dbname=$dbname user=$dbuser password=$dbpass");
+      if(!$dbh){
+        die("Error in connection: " . pg_last_error());
+      }
+
+      $data = pg_escape_string($_POST['data']);
+      $locid = pg_escape_string($_POST['loc_id']);
+
+      $sql = "INSERT INTO sensordata(loc_id,temp) VALUES('$locid', '$data')";
+      $result = pg_query($dbh, $sql);
+
+      if(!result){
+        die("Error in SQL query: " . pg_last_error());
+      }
+
+      echo "Data successfully inserted!";
+      pg_free_result($result);
+      pg_close($dbh);
+
     }
-
-    $data = pg_escape_string($_POST['data']);
-    $locid = pg_escape_string($_POST['loc_id']);
-
-    $sql = "INSERT INTO sensordata(loc_id,temp) VALUES('$locid', '$data')";
-    $result = pg_query($dbh, $sql);
-
-    if(!result){
-      die("Error in SQL query: " . pg_last_error());
-    }
-
-    echo "Data successfully inserted!";
-    pg_free_result($result);
-    pg_close($dbh);
-
   }
 ?>
